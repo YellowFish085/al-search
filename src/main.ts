0fc7ccf7b43d95f2e5140337c398984df6fc0041
Theo Costantini <dev@theocostantini.com>
2020-03-13 11:09:32 +0100

    Add FontAwesome

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faChevronDown, faCogs, faExternalLinkAlt, faSearch, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+
+// Setup Font Awesome available icons.
+library.add(faChevronDown, faCogs, faExternalLinkAlt, faSearch, faUserCircle);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
