46eb76e9c5447cf8f8958a5d539b5a0e969444ed
Theo Costantini <dev@theocostantini.com>
2020-04-10 15:57:31 +0200

    Change infos section

--- src/views/settings/Infos.vue
+++ src/views/settings/Infos.vue
@@ -1,44 +1,21 @@
 <template>
   <!-- eslint-disable max-len -->
   <div>
-    <h2>Informations about data</h2>
+    <h2>Informations</h2>
 
     <div class="container">
-      <h3>TL;DR</h3>
+      <h3>Data</h3>
       <p>We do not collect any of your data in any way, we don't care what you're whatching or reading. Just enjoy this extension :)</p>
 
       <h3>Activity</h3>
       <p>Search history and visited pages are stored locally, on your browser.</p>
       <p>This means that if you have <b>AniSearch</b> installed in multiple browsers on the same computer, or in multiple browsers on different computers, each <b>AniSearch</b> extension will have its own activity feed.</p>
-      <p class="note"><i>Of course, we do not collect any of these data.</i></p>
 
-      <h3>Account</h3>
-      <p>When you log in to your <b>AniList</b> account through <b>AniSearch</b>, we will use some of your data in the extension.</p>
-      <p>Here is the full list of what we use and how we use it:
-        <ul class="list">
-          <li>Your profile picture and user name
-            <ul>
-              <li>We use it to show you to which account your are logged in to.</li>
-            </ul>
-          </li>
-          <li>Link to your profile
-            <ul>
-              <li>We use it to give you a quick access to your profile page.</li>
-            </ul>
-          </li>
-          <li>Title language setting
-            <ul>
-              <li>We use it to display anime and manga titles in correct language inside <b>AniSearch</b>.</li>
-            </ul>
-          </li>
-          <li>Anime and manga lists content
-            <ul>
-              <li>We use them to display additional search results when a search is done inside <b>AniSearch</b>.</li>
-            </ul>
-          </li>
-        </ul>
-      </p>
-      <p class="note"><i>Of course, we do not collect any of these data.</i></p>
+      <h3>Links</h3>
+      <div class="links row row--justify-start row--items-center">
+        <a href="https://anilist.co" title="Visit AniList" class="anilist"></a>
+        <a href="https://github.com/YellowFish085/anisearch" title="Visit AniSearch repository" class="anisearch"></a>
+      </div>
     </div>
   </div>
   <!-- eslint-enable max-len -->
@@ -59,6 +36,26 @@ export default class Infos extends Vue {}
   ul {
     list-style-position: inside;
     padding-left: 1rem;
+  }
+}
+
+.links {
+  a {
+    background-color: rgb(var(--color-overlay));
+    background-position: center;
+    background-size: cover;
+    border-radius: 3px;
+    height: 50px;
+    margin-right: 10px;
+    width: 50px;
+
+    &.anilist {
+      background-image: url('https://anilist.co/img/icons/icon.svg');
+    }
+
+    &.anisearch {
+      background-image: url('../../../public/logo.svg');
+    }
   }
 }
 </style>
