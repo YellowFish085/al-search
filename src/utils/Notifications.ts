bfe59d2fef62e40f88a1424c8062a907b32ba4eb
Theo Costantini <dev@theocostantini.com>
2020-04-10 17:32:37 +0200

    Fix wrong image usage

--- src/utils/Notifications.ts
+++ src/utils/Notifications.ts
@@ -7,7 +7,7 @@ function create(id: string, message: string) {
   browser.notifications.create(id, {
     type: 'basic',
     title: 'AniSearch',
-    iconUrl: browser.extension.getURL('img/logo.png'),
+    iconUrl: browser.extension.getURL('img/logo.svg'),
     message,
   });
 }
