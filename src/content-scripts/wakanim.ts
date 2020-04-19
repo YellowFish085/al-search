2296fe37a818816907d224be3acb032cf4e410d9
Theo Costantini <dev@theocostantini.com>
2020-04-19 09:51:48 +0200

    Add missing web integration pages for Crunchyroll and Wakanim

--- src/content-scripts/wakanim.ts
+++ src/content-scripts/wakanim.ts
@@ -2,32 +2,43 @@ import WebIntegration from '@/utils/WebIntegration';
 import * as Enum from '@/utils/Enum';
 
 /**
- * Try to get an anime title from the page.
+ * Display overlay for anime page.
  */
-function getAnimeTitle(): string | null {
+function animePage(): void {
   const titleNode = document.querySelector('body > .SerieV2 meta[itemprop="alternativeHeadline"]');
+  const displayTitleNode = document.querySelector('body > .SerieV2 meta[itemprop="name"]');
 
-  return titleNode ? titleNode.getAttribute('content') : null;
+  const title = titleNode ? titleNode.getAttribute('content') : null;
+  const displayTitle = displayTitleNode ? displayTitleNode.getAttribute('content') : null;
+
+  if (title) WebIntegration.displayButton(title, Enum.SearchType.ANIME, displayTitle);
 }
 
 /**
- * Try to get an anime display name from the page.
+ * Display overlay for anime episode page.
  */
-function getDisplayTitle(): string | null {
-  const titleNode = document.querySelector('body > .SerieV2 meta[itemprop="name"]');
+function animeEpisodePage(): void {
+  const titleNode = document.querySelector('body > .episode .episode_info > .border-list > li:first-child > span.border-list_text') as HTMLElement;
+  const displayTitleNode = document.querySelector('body > .episode span[itemprop="partOfSeries"] meta[itemprop="name"]');
 
-  return titleNode ? titleNode.getAttribute('content') : null;
+  const title = titleNode ? titleNode.innerText : null;
+  const displayTitle = displayTitleNode ? displayTitleNode.getAttribute('content') : null;
+
+  if (title) WebIntegration.displayButton(title, Enum.SearchType.ANIME, displayTitle);
 }
 
 async function init() {
   try {
     if (!(await WebIntegration.isEnabled())) return;
 
-    // In case we are on a video page.
+    // In case we are on an anime page.
     if (document.querySelector('body > .SerieV2')) {
-      const title = getAnimeTitle();
+      animePage();
+    }
 
-      if (title) WebIntegration.displayButton(title, Enum.SearchType.ANIME, getDisplayTitle());
+    // In case we are on an anime episode page.
+    if (document.querySelector('body > .episode')) {
+      animeEpisodePage();
     }
   }
   catch (e) {
