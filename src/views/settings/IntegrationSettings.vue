8299acd351e2a14e6d3d21e87fab1b86b85b8ec7
Theo Costantini <dev@theocostantini.com>
2020-04-25 17:16:43 +0200

    Small changes in web integration settings component

--- src/views/settings/IntegrationSettings.vue
+++ src/views/settings/IntegrationSettings.vue
@@ -13,7 +13,7 @@
           <label for="settings__integration__web_enabled">{{ i18n('S_EnableWebIntegration') }}</label>
         </div>
 
-        <div :class="{ hidden: !webEnabled }">
+        <div class="subfield" :class="{ hidden: !webEnabled }">
           <div class="field">
             <input type="checkbox"
                   name="overlay_near_title"
@@ -22,22 +22,23 @@
             <label for="settings__integration__overlay_near_title">{{ i18n('S_OverlayNearTitle') }}</label>
           </div>
 
-          <div class="settings__integration__overlay_position row row--justify-start row--items-start" :class="{ hidden: inPage}">
-            <div class="field field--col">
-              <label for="settings__integration__overlay_x">{{ i18n('S_OverlayX') }}</label>
+          <div class="settings__integration__overlay_position subfield col col--justify-start col--items-start" :class="{ hidden: inPage}">
+            <p class="note">{{ i18n('S_OverlayPositionFixed') }}</p>
+            <div class="field">
               <select name="overlay_x"
                       id="settings__integration__overlay_x"
                       v-model="x">
                 <option v-for="xValue in xValues" :key="xValue" :value="xValue">{{ i18n(`ENUM_${xValue}`) }}</option>
               </select>
+              <label for="settings__integration__overlay_x">{{ i18n('S_OverlayX') }}</label>
             </div>
-            <div class="field field--col">
-              <label for="settings__integration__overlay_y">{{ i18n('S_OverlayY') }}</label>
+            <div class="field">
               <select name="overlay_y"
                       id="settings__integration__overlay_y"
                       v-model="y">
                 <option v-for="yValue in yValues" :key="yValue" :value="yValue">{{ i18n(`ENUM_${yValue}`) }}</option>
               </select>
+              <label for="settings__integration__overlay_y">{{ i18n('S_OverlayY') }}</label>
             </div>
           </div>
         </div>
@@ -149,24 +150,8 @@ export default class IntegrationSettings extends Mixins(Vue, MixinI18n) {
   display: none;
 }
 
-.settings__integration__overlay_position {
-  margin-top: 1.2rem;
-
-  .field {
-    margin-left: 0.3rem;
-    margin-right: 1rem;
-  }
-}
-
 .field {
   margin-bottom: 0.25rem;
-
-  &--col {
-    label {
-      display: block;
-      margin-left: 0;
-    }
-  }
 }
 
 input {
@@ -176,6 +161,33 @@ input {
 label {
   margin-left: 0.5rem;
   font-size: 1.4rem;
+}
+
+.subfield {
+  margin-left: 2rem;
+
+  label {
+    font-size: 1.2rem;
+  }
+}
+
+.settings__integration__overlay_position {
+  p {
+    margin: 0.5rem 0;
+  }
+
+  .field {
+    width: 100%;
+
+    select {
+      width: 30%;
+    }
+
+    label {
+      color: rgb(var(--color-text-light));
+      font-style: italic;
+    }
+  }
 }
 
 </style>
