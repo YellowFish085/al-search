a2b5a252df1119dfd4b20809776825dc743d2566
thesupersonic16 <supersonic16bit@gmail.com>
2020-10-12 19:12:13 +1100

    Fixed some issues with AnimeLab integration

--- src/content-scripts/animelab.ts
+++ src/content-scripts/animelab.ts
@@ -1,33 +1,40 @@
 /* eslint-disable no-param-reassign */
 import create from '@/content-scripts/web-integration';
+import Settings from '@/utils/Settings';
 
 /**
  * Display overlay for anime page.
  */
 async function animePage(): Promise<void> {
+  const settings = await Settings.getSettings();
+
   await create({
     selector: '.show-title',
     appendInPage(node: HTMLElement): void {
       node.style.marginBottom = '10px';
+      node.style.marginTop = '-30px';
 
-      // Insert node before title.
+      // Insert node before title
       const target = document.querySelector('.show-title');
       if (target) target.parentNode!.insertBefore(node, target);
     },
   });
 
-  // Move the uservoice button
-  const uservoiceNode = document.getElementById('uservoice-trigger');
-
   // Move the uservoice button for consistency
-  if (uservoiceNode) {
-    uservoiceNode.style.right = '70px';
-    uservoiceNode.style.bottom = '14px';
+  if (!settings.integration.overlay.inPage
+   && settings.integration.overlay.x === 'RIGHT'
+   && settings.integration.overlay.y === 'BOTTOM') {
+    const uservoiceNode = document.getElementById('uservoice-trigger');
+
+    if (uservoiceNode) {
+      uservoiceNode.style.right = '70px';
+      uservoiceNode.style.bottom = '14px';
+    }
   }
 }
 
 function init(): void {
-  // In case we are on an anime page.
+  // In case we are on an anime page
   if (document.querySelector('.show-details')) {
     animePage();
   }
